7ceb430e38b7d4ed31483243dbc9393711f31b2b
STUDZPSB\krak228 <kryrak2@gmail.com>
2021-11-25 16:57:07 +0100

    generator przeszkód

---
+++ Assets/Generator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+
+    public GameObject przeszkoda;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void StworzPrzeszkode()
+    {
+
+    }
+}   
